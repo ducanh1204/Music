ffd51837b35acfdd148a800b170229722a182e1d
Duc Anh <nguyenducanh12042000@gmail.com>
2019-11-10 09:40:35 +0700

    a

--- app/src/main/java/vn/edu/poly/music/Adapter/SongAdapter.java
+++ app/src/main/java/vn/edu/poly/music/Adapter/SongAdapter.java
@@ -7,9 +7,11 @@ import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -35,7 +37,6 @@ public class SongAdapter extends RecyclerView.Adapter<SongAdapter.SongHolder> {
     private SongDAO songDAO;
 
 
-
     @NonNull
     @Override
     public SongHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -48,45 +49,47 @@ public class SongAdapter extends RecyclerView.Adapter<SongAdapter.SongHolder> {
         songDAO = new SongDAO(context);
         holder.tvtenBaiHat.setText(songList.get(position).getTenBaiHat());
         holder.tvtenCaSi.setText(songList.get(position).getTenCaSi());
-        holder.imgDelete_Song.setOnClickListener(new View.OnClickListener() {
+        holder.imgPopupmenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(context);
-                builder.setMessage("Bạn có chắc chắn muốn xóa bài hát này không?");
-                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                PopupMenu popupMenu = new PopupMenu(context,v);
+                popupMenu.getMenuInflater().inflate(R.menu.popup_menu,popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
                     @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        songDAO.delete(songList.get(position).getTenBaiHat());
-                        songList.remove(position);
-                        notifyDataSetChanged();
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()){
+                            case R.id.itemSua:
+                                break;
+                            case R.id.itemXoa:
+                                createAlertDialag(position);
+                                break;
+                        }
+                        return false;
                     }
                 });
-                builder.setNegativeButton("Hủy", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
+                popupMenu.show();
+            }
+        });
 
-                    }
-                });
-                builder.create().show();
-            }
-        });
-        holder.imgAdd_playlist.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (MainActivity.mediaPlayer.isPlaying()) {
-                    MainActivity.mediaPlayer.stop();
-                }
-            }
-        });
+
+
+//        holder.imgAdd_playlist.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (MainActivity.mediaPlayer.isPlaying()) {
+//                    MainActivity.mediaPlayer.stop();
+//                }
+//            }
+//        });
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                start(position);
+                startSong(position);
             }
         });
     }
 
-    private void start(int position) {
+    private void startSong(int position) {
         MainActivity.mediaPlayer = new MediaPlayer();
         MainActivity.mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
         Uri uri = Uri.parse(songList.get(position).getFileMp3());
@@ -99,6 +102,26 @@ public class SongAdapter extends RecyclerView.Adapter<SongAdapter.SongHolder> {
         }
     }
 
+    private void createAlertDialag(final int position) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setMessage("Bạn có chắc chắn muốn xóa bài hát này không?");
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                songDAO.delete(songList.get(position).getTenBaiHat());
+                songList.remove(position);
+                notifyDataSetChanged();
+            }
+        });
+        builder.setNegativeButton("Hủy", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.create().show();
+    }
+
     @Override
     public int getItemCount() {
         return songList.size();
@@ -106,14 +129,13 @@ public class SongAdapter extends RecyclerView.Adapter<SongAdapter.SongHolder> {
 
     public class SongHolder extends RecyclerView.ViewHolder {
         private TextView tvtenBaiHat, tvtenCaSi;
-        private ImageView imgAdd_playlist, imgDelete_Song;
+        private ImageView imgPopupmenu;
 
         public SongHolder(@NonNull View itemView) {
             super(itemView);
             tvtenBaiHat = itemView.findViewById(R.id.tvtenBaiHat);
             tvtenCaSi = itemView.findViewById(R.id.tvtenCaSi);
-            imgAdd_playlist = itemView.findViewById(R.id.imgAdd_playlist);
-            imgDelete_Song = itemView.findViewById(R.id.imgDelete_Song);
+            imgPopupmenu = itemView.findViewById(R.id.imgPopupMenu);
         }
     }
 }
